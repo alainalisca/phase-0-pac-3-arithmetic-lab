2b2432c9fa7c8df61691c6f4ed98899abcb5a14f
Alain Alisca <alainalisca@gmail.com>
2022-02-11 15:43:23 -0500

    commit tree 17777fb162d1ea24d24800cf772382769bc9d857

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//5
+function add(a, b){
+    return (a + b);
+}
+//6
+function subtract(a, b){
+    return (a - b);
+}
+//7
+function multiply(a, b){
+    return (a * b);
+}
+//8
+function divide(a, b) {
+    return (a / b);
+}
+//9
+function increment(a){
+    a ++;
+    return (a);
+} 
+//10
+function decrement(a) {
+    a --; 
+    return (a);
+}
+//1
+function makeInt(n){
+   return parseInt(n, 10);
+}
+
+//4
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+
